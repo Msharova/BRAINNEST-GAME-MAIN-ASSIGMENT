88661f7d87b199ed01c53095c6b2dd479830f025
Maria Sharova <sharova.maria.3d@gmail.com>
2023-02-10 20:47:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 function computerPlay(){
-    //created an array of needed words
     const gameWords =[`Rock`, `Paper`, `Scissors`]; 
-    //Math.floor - rounds down to the nearest integer
-    //Math.random - gives the random number between 0 and 1
-    //gamewords.lenght - the length of the array gameWords (will return 3)
-    //then it means we multiply our random on 3 and now it shows randoms floats from 0 to 3
-    //then we use Math.floor to round down to integers
-    //then we return the index of the array
     return gameWords[Math.floor(Math.random() * gameWords.length)];
 }
-//the function of all game rounds that returns the result of the comparison
+
 function playRound(playerSelection, computerSelection){    
     switch (playerSelection) {
         case `rock`:
@@ -31,13 +24,7 @@ function playRound(playerSelection, computerSelection){
             return noGame;
     }
 }
-//firstly we define the counters and set it on 0
-//then we do the loop of N games, where N is defined when we CALL for game() funcion
-//then we define constants of what player and computer decided to input and lowercase them in order to compare
-//then we add the console.log of round counts in the loop
-//then we define result and place there a function that will count win/lose/tie and sends playerSelection and computerSelection parameters
-//then we count win/lose
-//after the loop we decide what to write in console depending on our score
+
 function game(roundCount){
     let playerWin = 0;
     let compWin = 0;
@@ -46,7 +33,6 @@ function game(roundCount){
         const questionForPlayer = prompt(`Let's play! Write down "Rock", "Paper" or "Scissors"`);
         const playerSelection = questionForPlayer.toLowerCase();
         const computerSelection = computerPlay().toLowerCase();
-        //const computerSelection = `paper`; //we need this only to control the situation
 
         console.log(`Round ${i+1}: Computer selected: ${capitalizeFirstLetter(computerSelection)}, you selected ${capitalizeFirstLetter(playerSelection)}`);
 
@@ -65,11 +51,11 @@ function game(roundCount){
         console.log(`You played ${roundCount} games, won ${playerWin}, lost ${compWin}. It's a tie!`);
     }
 }
-//we use this function to capitalize only the first letter
+
 function capitalizeFirstLetter(wordSelected){
     return wordSelected.charAt(0).toUpperCase() + wordSelected.slice(1);
 }
-//globals
+
 let winGame = `You win!`;
 let loseGame = `You lose!`;
 let tieGame = `It's a tie!`;
